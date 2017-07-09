7ee636a355d0d5fe5472828d9869fdcfcd1aca47
nikcsit <nikhil_deshmukh@srmuniv.edu.in>
2017-07-09 12:04:52 +0530

    Add files via upload

---
+++ odd and even.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+	 int x;
+	 Scanner s= new Scanner(System.in);
+	 System.out.println("check even and odd-");
+	 System.out.println("enter value:");
+	 x=s.nextInt();
+	 
+	 if(x%2==0)
+	 {
+	 	System.out.println("even");
+	 }
+	 else  
+	 {
+	 	System.out.println("odd");
+	 }
+	 
+	 
+	}
+}
